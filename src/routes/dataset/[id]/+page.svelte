503b87aeef31dbc59f465b0b695b8bfcff371d40
mcaceres2017 <matias.guido.caceres1998@gmail.com>
2023-11-23 21:12:40 -0300

    Dataset details page done

--- src/routes/dataset/[id]/+page.svelte
+++ src/routes/dataset/[id]/+page.svelte
@@ -4,136 +4,74 @@
 	import type { PageData } from './$types';
 	export let data: PageData;
 	const { dataset } = data;
+
+	let selectedVersion = dataset.version[0];
 </script>
 
-<div class="my-container">
-	<div class="my-row">
-		<div class="my-col">
-			<div class="card p-4 title-card">
-				<p class="icon-text">
-					<Fa icon={faBox} size="4x" />
-				</p>
+<div class="my-container h-full mx-auto flex justify-center items-center">
+	<div class="card card-metadata p-4">
+		<div class="my-row">
+			<Fa icon={faBox} size="4x" style="margin-left:30px;" />
+			<div class="my-col">
 				<div class="title">
 					{dataset.title} a little more of text to see what the div does
 				</div>
 			</div>
 		</div>
 		<div class="my-col">
-			<div class="my-row">
-				<div class="card p-4 general-info overflow-auto max-h-[200px]">
-					<div><strong>Author:</strong> {dataset.author}</div>
-					<div><strong>Created:</strong> {dataset.created}</div>
-					<div>
-						<strong>Description:</strong>
-						{dataset.description}
-					</div>
-				</div>
-			</div>
-			<div class="my-row btn-row">
-				<div>
-					<button class="btn variant-filled">Update</button>
-				</div>
-				<div>
-					<button class="btn variant-filled">Update</button>
-				</div>
-			</div>
-			<div class="my-row">
-				<div class="card p-4 version-info">
-					<div class="version-selector">
-						<strong>Version:</strong>
+			<div class="general-info">
+				<div class="title-2">General</div>
+				<ul class="description-list">
+					<li>
+						<strong>Author:</strong>
+						{dataset.author}
+					</li>
 
-						<select class="select">
-							{#each dataset.version as version}
-								<option value={version}>{version}</option>
-							{/each}
-						</select>
-					</div>
-					<div><strong>Updated:</strong> {dataset.updated}</div>
-					<div><strong>Oids:</strong> {dataset.oids.join(', ')}</div>
-					<div><strong>Parameters:</strong> {dataset.parameters.join(', ')}</div>
-					<div><strong>Size:</strong> {dataset.size}</div>
+					<li>
+						<strong>Creation date:</strong>
+						{dataset.created}
+					</li>
+
+					<li>
+						<strong>Description:</strong>
+						{dataset.description} aaaaaaaaaa fd fd fds fsdaa afd afsd fs dfsda fsda f
+					</li>
+				</ul>
+			</div>
+
+			<div class="version-info">
+				<div class="title-2">Version Specific Information</div>
+
+				<strong>Select a version:</strong>
+
+				<div style="width:80px; margin-top:10px; margin-bottom:10px;">
+					<select class="select" bind:value={selectedVersion}>
+						{#each dataset.version as version}
+							<option value={version}>{version}</option>
+						{/each}
+					</select>
 				</div>
+
+				<ul class="description-list">
+					<li><strong>Updated on:</strong> {dataset.updated}</li>
+					<li><strong>Oids:</strong> {dataset.oids.join(', ')}</li>
+					<li><strong>Parameters:</strong> {dataset.parameters.join(', ')}</li>
+					<li><strong>Size:</strong> {dataset.size}</li>
+				</ul>
+			</div>
+
+			<div class="btn-row">
+				<button type="button" class="btn variant-filled-primary">Get full metadata</button>
+				<button type="button" class="btn variant-filled-secondary">Update this dataset</button>
+				<button type="button" class="btn variant-filled-tertiary"
+					>Download version {selectedVersion}</button
+				>
 			</div>
 		</div>
 	</div>
 </div>
 
-<!-- <div class="center-container">
-	<div class="card p-4">
-		<div class="info-container" />
-		<p class="icon-text">
-			<Fa icon={faBox} size="4x" />
-		</p>
-		<div class="title">{dataset.title} a little more of text to see what the div does</div>
-
-		<div>
-			<strong>Version:</strong>
-
-			<select class="select">
-				{#each dataset.version as version}
-					<option value={version}>Version {version}</option>
-				{/each}
-			</select>
-		</div>
-
-		## <div><strong>Updated:</strong> {dataset.updated}</div>
-
-		## <div><strong>Created:</strong> {dataset.created}</div>
-
-		## <div><strong>Author:</strong> {dataset.author}</div>
-
-		## <div><strong>Oids:</strong> {dataset.oids.join(', ')}</div>
-
-		## <div><strong>Parameters:</strong> {dataset.parameters.join(', ')}</div>
-
-		## <div><strong>Size:</strong> {dataset.size}</div>
-
-		<div>
-			<button class="btn variant-filled">Update</button>
-		</div>
-	</div>
-</div> -->
-
 <style>
-	.btn {
-		margin-left: 100px;
-	}
-
-	.title {
-		font-size: 25px;
-		font-weight: bold;
-		margin-bottom: 20px;
-	}
-
-	.icon-text {
-		justify-content: center;
-		margin-right: 15px;
-		align-items: center;
-	}
-
-	.title-card {
-		max-width: 250px;
-		max-height: 1000px;
-		margin-top: 80px;
-		margin-left: 100px;
-		align-items: center;
-		justify-content: center;
-	}
-
-	.general-info {
-		width: 800px;
-		height: 200px;
-		margin-top: 80px;
-		margin-left: 100px;
-	}
-
-	.version-info {
-		width: 800px;
-		height: 200px;
-		margin-top: 20px;
-		margin-left: 100px;
-	}
-
 	.my-container {
 		max-width: 1200px;
 		margin: 0 auto;
@@ -150,22 +88,40 @@
 		padding: 0 15px;
 	}
 
-	.version-selector {
-		display: flex;
-		align-items: center;
-		margin-bottom: 20px;
+	.card-metadata {
+		margin-top: 30px;
+		max-width: 800px;
 	}
 
-	.select {
-		margin-left: 20px;
-		width: 100px;
+	.title {
+		margin-left: 15px;
+		font-size: 24px;
+		text-align: center;
+		font-weight: bold;
+		text-align: justify;
+	}
+
+	.title-2 {
+		margin-top: 20px;
+		font-size: 18px;
+		margin-bottom: 20px;
+		text-align: left;
+		font-weight: bold;
+	}
+
+	.description-list {
+		font-size: 14px;
+		color: #cacaca;
+		text-align: justify;
+		text-justify: inter-word;
 	}
 
 	.btn-row {
 		display: flex;
 		align-items: center;
 		justify-content: center;
-		margin-left: 85px;
-		background-color: rgb(124, 125, 204);
+		gap: 50px;
+		flex-wrap: wrap;
+		margin-top: 20px;
 	}
 </style>
